c10013f1592c0cab55c864caa5c2cc73cb015eb3
Giangiacomo Sanna <giangiacomo.sanna@gmail.com>
2017-02-06 22:58:00 +0100

    setup x and y values

--- plot_proj.py
+++ plot_proj.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 available_functions={ 1: ("x -> x", lambda x: x)
                       }
@@ -9,3 +10,15 @@ f = available_functions[int(sys.argv[1])][1]
 # Test handling command line input
 # test_val = 10
 # print "The identity function in " + str(test_val) + " is: " + str(f(10))
+
+# setup x and y axes
+start=-5.0
+end=5.0
+interval=0.1
+nsteps=(end-start + 0.0)/interval + 1
+# abs((nsteps-1)*interval-(end-start))<tol
+xval=np.linspace(start,end,nsteps)
+yval=f(xval)
+
+# Test creation of xval and yval
+print xval[10:20], yval[10:20]
