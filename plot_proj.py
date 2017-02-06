908b7e5522c12f8b812bef9816564654d8e88072
Giangiacomo Sanna <giangiacomo.sanna@gmail.com>
2017-02-06 23:58:57 +0100

    usage message is prettier now

--- plot_proj.py
+++ plot_proj.py
@@ -17,28 +17,22 @@ usage_message="Usage: python " + str(sys.argv[0]) + " <n> \n" \
              + "\"n\" is an integer. The function corresponding to it is:"
 if(len(sys.argv) == 1):
     print usage_message
+    print "\n" + "n | function "
     for key in available_functions:
-        print str(key) + ": " + available_functions[key][0]
+        print str(key) + " : " + available_functions[key][0]
+    print ""
     sys.exit()
 
 # set f according to input
 f = available_functions[int(sys.argv[1])][1]
-
-# Test handling command line input
-# test_val = 10
-# print "The identity function in " + str(test_val) + " is: " + str(f(10))
 
 # setup x and y axes
 start=-3.0
 end=3.0
 interval=0.1
 nsteps=(end-start + 0.0)/interval + 1
-# abs((nsteps-1)*interval-(end-start))<tol
 xval=np.linspace(start,end,nsteps)
 yval=f(xval)
-
-# Test creation of xval and yval
-# print xval[10:20], yval[10:20]
 
 # plot function
 plt.plot(xval,yval)
